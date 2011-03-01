281fdac44dd768249eca009ef2f037198f28dc8e
Shelby Munsch <smunsch@gmail.com>
2011-03-01 01:23:47 -0800

    Changed some JS to PHP

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,14 +5,14 @@
 	<script type="text/javascript" src="js/jquery.min.js"></script>
 	<script type="text/javascript" src="js/jquery-ui.min.js"></script>
 	<script type="text/javascript">
-        function setCookie(cookie_name, value, expiration)
-        {
-        	var exdate = new Date();
-        	exdate.setDate(exdate.getDate() + expiration)
-        	var cookie_val = escape(value) + ((epiration==null) ? "":"; expires="+exdate.toUTCString());
-      		document.cookie=cookie_name + "=" + cookie_val;
-      	}
-      	
+	function setCookie(cookie_name, value, expiration)
+	{
+		var exdate = new Date();
+		exdate.setDate(exdate.getDate() + expiration)
+		var cookie_val = escape(value) + ((epiration==null) ? "":"; expires="+exdate.toUTCString());
+		document.cookie=cookie_name + "=" + cookie_val;
+	}
+
 	function getCookie(cookie_name)
 	{
 		var i,x,y,ARRcookies=document.cookie,split(";");
@@ -22,7 +21,7 @@
 			x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
 			y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
 			x = x.replace(/^\s+|\s+$/g,"");
-			
+
 			if (x == cookie_name)
 			{
 				return unescape(y);
@@ -31,38 +30,38 @@
 	}
 	</script>
 	<link rel="stylesheet" type="text/css" href="css/default.css">
-	<style type="text/css"> body { background-image: url(../<script type="test/javascript">getCookie(bg)</script>); } </style>
+	<style type="text/css"> body { background-image: url(uploads/<?php echo $_COOKIE["bg"]; ?>); } </style>
 </head>
 <body style="cursor: auto;">
 <script type="text/javascript">
-	var display = true;
-	$(document).ready(function(){
+var display = true;
+$(document).ready(function(){
+	$("body").css("background-image", "url(uploads/hobo.gif)");
+	$(".1").click(function(){
 		$("body").css("background-image", "url(uploads/hobo.gif)");
-		$(".1").click(function(){
-			$("body").css("background-image", "url(uploads/hobo.gif)");
-			$("ul.wall").children().removeClass("selected");
-			$(".1").addClass("selected");
-			setCookie("bg", "hobo.gif");
-		});
-		$(".2").click(function(){
-			$("body").css("background-image", "url(uploads/2.jpg)");
-			$("ul.wall").children().removeClass("selected");
-			$(".2").addClass("selected");
-			setCookie("bg", "2.jpg");
-		});
-		$(".3").click(function(){
-			$("body").css("background-image", "url(uploads/3.jpg)");
-			$("ul.wall").children().removeClass("selected");
-			$(".3").addClass("selected");
-			setCookie("bg", "3.jpg");
-		});
-		$(".4").click(function(){
-			$("body").css("background-image", "url(uploads/4.jpg)");
-			$("ul.wall").children().removeClass("selected");
-			$(".4").addClass("selected");
-			setCookie("bg", "4.jpg");
-		});
+		$("ul.wall").children().removeClass("selected");
+		$(".1").addClass("selected");
+		setCookie("bg", "hobo.gif");
 	});
+	$(".2").click(function(){
+		$("body").css("background-image", "url(uploads/2.jpg)");
+		$("ul.wall").children().removeClass("selected");
+		$(".2").addClass("selected");
+		setCookie("bg", "2.jpg");
+	});
+	$(".3").click(function(){
+		$("body").css("background-image", "url(uploads/3.jpg)");
+		$("ul.wall").children().removeClass("selected");
+		$(".3").addClass("selected");
+		setCookie("bg", "3.jpg");
+	});
+	$(".4").click(function(){
+		$("body").css("background-image", "url(uploads/4.jpg)");
+		$("ul.wall").children().removeClass("selected");
+		$(".4").addClass("selected");
+		setCookie("bg", "4.jpg");
+	});
+});
 </script>
 <div class="title" id="title">
 	<h1>Files... upload/browse</h1>
@@ -82,38 +81,38 @@
 <div class="block">
 	<div id="browse">
 		<?php
-			function showContent($path){
+	function showContent($path){
 
-				if ($handle = opendir($path))
+		if ($handle = opendir($path))
+		{
+			$up = substr($path, 0, (strrpos(dirname($path."/."),"/")));
+			while (false !== ($file = readdir($handle)))
+			{
+				if ($file != "." && $file != "..")
 				{
-					$up = substr($path, 0, (strrpos(dirname($path."/."),"/")));
-					while (false !== ($file = readdir($handle)))
-					{
-						if ($file != "." && $file != "..")
-						{
-							$fName  = $file;
-							$file   = $path.'/'.$file;
-							$ext = substr($fName, strrpos($fName, '.') + 1);
-							$images = array("jpg", "png", "jpeg", "gif");
-							if (!is_dir($file)) {
-								if (in_array(strtolower($ext), $images)) {
-									// Image handler!
-									echo "<div id='img'><a href='".$file."' class='img'><img src='".$file."'><p class='caption'>".$fName."</p></a></div>";
-								} else {
-									if(is_file($file) && $fName != ".DS_Store") {
-										echo "<div class='file'><a href='".$file."'>".$fName."</a></div>";
-									}
-								}
+					$fName  = $file;
+					$file   = $path.'/'.$file;
+					$ext = substr($fName, strrpos($fName, '.') + 1);
+					$images = array("jpg", "png", "jpeg", "gif");
+					if (!is_dir($file)) {
+						if (in_array(strtolower($ext), $images)) {
+							// Image handler!
+							echo "<div id='img'><a href='".$file."' class='img'><img src='".$file."'><p class='caption'>".$fName."</p></a></div>";
+						} else {
+							if(is_file($file) && $fName != ".DS_Store") {
+								echo "<div class='file'><a href='".$file."'>".$fName."</a></div>";
 							}
 						}
 					}
-					closedir($handle);
-				}    
+				}
 			}
-			showContent("uploads");
-		?>
-		<br style="clear:both;">
-	</div>
+			closedir($handle);
+		}    
+	}
+	showContent("uploads");
+	?>
+	<br style="clear:both;">
+</div>
 </div>
 
 <ul class="wall">
@@ -126,17 +125,17 @@
 
 <script src="js/fileuploader.js" type="text/javascript"></script>
 <script>        
-    function createUploader(){            
-        var uploader = new qq.FileUploader({
-            element: document.getElementById('file-uploader'),
-            action: 'fileuploader.php',
-            debug: true
-        });           
-    }
-    
-    // in your app create uploader as soon as the DOM is ready
-    // don't wait for the window to load  
-    window.onload = createUploader;     
+function createUploader(){            
+	var uploader = new qq.FileUploader({
+		element: document.getElementById('file-uploader'),
+		action: 'fileuploader.php',
+		debug: true
+	});           
+}
+
+// in your app create uploader as soon as the DOM is ready
+// don't wait for the window to load  
+window.onload = createUploader;     
 </script>
 
 </body></html>
