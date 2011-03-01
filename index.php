c0d29e25c0049aba8b1bef8f26835d07f428a543
Shelby Munsch <smunsch@gmail.com>
2011-03-01 02:00:31 -0800

    implemented cookie cache for wall setter

--- index.php
+++ index.php
@@ -5,63 +5,87 @@
 	<script type="text/javascript" src="js/jquery.min.js"></script>
 	<script type="text/javascript" src="js/jquery-ui.min.js"></script>
 	<script type="text/javascript">
-	function setCookie(cookie_name, value, expiration)
-	{
-		var exdate = new Date();
-		exdate.setDate(exdate.getDate() + expiration)
-		var cookie_val = escape(value) + ((epiration==null) ? "":"; expires="+exdate.toUTCString());
-		document.cookie=cookie_name + "=" + cookie_val;
+	function setCookie(name,value,days) {
+		if (days) {
+			var date = new Date();
+			date.setTime(date.getTime()+(days*24*60*60*1000));
+			var expires = "; expires="+date.toGMTString();
+		}
+		else var expires = "";
+		document.cookie = name+"="+value+expires+"; path=/";
 	}
 
-	function getCookie(cookie_name)
-	{
-		var i,x,y,ARRcookies=document.cookie,split(";");
-		for (i=0; i < ARRcookies.length; i++)
-		{
-			x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
-			y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
-			x = x.replace(/^\s+|\s+$/g,"");
-
-			if (x == cookie_name)
-			{
-				return unescape(y);
-			}
+	function readCookie(name) {
+		var nameEQ = name + "=";
+		var ca = document.cookie.split(';');
+		for(var i=0;i < ca.length;i++) {
+			var c = ca[i];
+			while (c.charAt(0)==' ') c = c.substring(1,c.length);
+			if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
 		}
+		return null;
 	}
 	</script>
 	<link rel="stylesheet" type="text/css" href="css/default.css">
-	<style type="text/css"> body { background-image: url(uploads/<?php echo $_COOKIE["bg"]; ?>); } </style>
+	<?php
+		if (isset($_COOKIE["bg"]))
+		{
+			echo '<style type="text/css"> body { background-image: url(uploads/';
+			echo $_COOKIE["bg"];
+			echo '.jpg); } </style>';
+		}
+		else
+		{
+			echo '<style type="text/css"> body { background-image: url(uploads/1.jpg); } </style>';
+		}
+		function bgs($input)
+		{
+			if (isset($_COOKIE["bg"]))
+			{
+				if ($_COOKIE["bg"] == $input)
+				{
+					echo "selected";
+				}
+			} else {
+				if($input=="1")
+				{
+					echo "selected";
+				} 
+			}
+		}
+	?>
 </head>
 <body style="cursor: auto;">
 <script type="text/javascript">
 var display = true;
+
 $(document).ready(function(){
-	$("body").css("background-image", "url(uploads/hobo.gif)");
 	$(".1").click(function(){
-		$("body").css("background-image", "url(uploads/hobo.gif)");
+		$("body").css("background-image", "url(uploads/1.jpg)");
 		$("ul.wall").children().removeClass("selected");
 		$(".1").addClass("selected");
-		setCookie("bg", "hobo.gif");
+		setCookie("bg", "1", 3600);
 	});
 	$(".2").click(function(){
 		$("body").css("background-image", "url(uploads/2.jpg)");
 		$("ul.wall").children().removeClass("selected");
 		$(".2").addClass("selected");
-		setCookie("bg", "2.jpg");
+		setCookie("bg", "2", 3600);
 	});
 	$(".3").click(function(){
 		$("body").css("background-image", "url(uploads/3.jpg)");
 		$("ul.wall").children().removeClass("selected");
 		$(".3").addClass("selected");
-		setCookie("bg", "3.jpg");
+		setCookie("bg", "3", 3600);
 	});
 	$(".4").click(function(){
 		$("body").css("background-image", "url(uploads/4.jpg)");
 		$("ul.wall").children().removeClass("selected");
 		$(".4").addClass("selected");
-		setCookie("bg", "4.jpg");
+		setCookie("bg", "4", 3600);
 	});
 });
+
 </script>
 <div class="title" id="title">
 	<h1>Files... upload/browse</h1>
@@ -81,46 +105,46 @@ $(document).ready(function(){
 <div class="block">
 	<div id="browse">
 		<?php
-	function showContent($path){
+			function showContent($path){
 
-		if ($handle = opendir($path))
-		{
-			$up = substr($path, 0, (strrpos(dirname($path."/."),"/")));
-			while (false !== ($file = readdir($handle)))
-			{
-				if ($file != "." && $file != "..")
+				if ($handle = opendir($path))
 				{
-					$fName  = $file;
-					$file   = $path.'/'.$file;
-					$ext = substr($fName, strrpos($fName, '.') + 1);
-					$images = array("jpg", "png", "jpeg", "gif");
-					if (!is_dir($file)) {
-						if (in_array(strtolower($ext), $images)) {
-							// Image handler!
-							echo "<div id='img'><a href='".$file."' class='img'><img src='".$file."'><p class='caption'>".$fName."</p></a></div>";
-						} else {
-							if(is_file($file) && $fName != ".DS_Store") {
-								echo "<div class='file'><a href='".$file."'>".$fName."</a></div>";
+					$up = substr($path, 0, (strrpos(dirname($path."/."),"/")));
+					while (false !== ($file = readdir($handle)))
+					{
+						if ($file != "." && $file != "..")
+						{
+							$fName  = $file;
+							$file   = $path.'/'.$file;
+							$ext = substr($fName, strrpos($fName, '.') + 1);
+							$images = array("jpg", "png", "jpeg", "gif");
+							if (!is_dir($file)) {
+								if (in_array(strtolower($ext), $images)) {
+									// Image handler!
+									echo "<div id='img'><a href='".$file."' class='img'><img src='".$file."'><p class='caption'>".$fName."</p></a></div>";
+								} else {
+									if(is_file($file) && $fName != ".DS_Store") {
+										echo "<div class='file'><a href='".$file."'>".$fName."</a></div>";
+									}
+								}
 							}
 						}
 					}
-				}
+					closedir($handle);
+				}    
 			}
-			closedir($handle);
-		}    
-	}
-	showContent("uploads");
-	?>
-	<br style="clear:both;">
-</div>
+			showContent("uploads");
+		?>
+		<br style="clear:both;">
+	</div>
 </div>
 
 <ul class="wall">
 	<li>wall:</li>
-	<li class="1 selected">1</li>
-	<li class="2">2</li>
-	<li class="3">3</li>
-	<li class="4">4</li>
+	<li class="1 <?php bgs("1"); ?>">1</li>
+	<li class="2 <?php bgs("2"); ?>">2</li>
+	<li class="3 <?php bgs("3"); ?>">3</li>
+	<li class="4 <?php bgs("4"); ?>">4</li>
 </ul>
 
 <script src="js/fileuploader.js" type="text/javascript"></script>
